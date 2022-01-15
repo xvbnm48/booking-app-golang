03139eaa0cb1df7c9a4d956b8d01d67c502e7355
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-01-15 15:00:25 +0700

    variabel

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("welcome to our comference booking application")
+	var conferenceName = "GO Conference"
+	fmt.Println("welcome to our", conferenceName, "booking application")
 	fmt.Println("get your tickets here to attend")
+
+	fmt.Println(conferenceName)
 }
