d081719ec79f5d81f4568f728653b6351485b091
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-01-15 14:53:04 +0700

    go dasar

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/xvbnm48/booking-app-golang
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("welcome to our comference booking application")
+	fmt.Println("get your tickets here to attend")
+}
