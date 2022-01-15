25619adb6559e09a933d0de043aa100fca93f2db
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-01-15 15:06:55 +0700

    menggunakan variabel const

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import "fmt"
 
 func main() {
 	var conferenceName = "GO Conference"
+	const conferenTickets = 50
+	var remainingTickets = 50
 	fmt.Println("welcome to our", conferenceName, "booking application")
+	fmt.Println("we have total of ,", conferenTickets, "tickets and ,", remainingTickets, "are stil available")
 	fmt.Println("get your tickets here to attend")
 
 	fmt.Println(conferenceName)
